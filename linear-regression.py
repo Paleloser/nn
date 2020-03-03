a154615af53f4ba0e70772ea5c0a57b44fb72303
Paleloser <pablo.caraballorente@gmail.com>
2020-03-03 01:13:55 +0100

    feat: first design. TODO: explain matrix ops.

---
+++ linear-regression.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_boston
+
+#                      //////////////
+#                      //          //
+#  [X_0] --- [W_0] --- //  NEURON  // --- [Y_0]
+#              ^       //          //
+#              |       //////////////
+#          <unknown>         |
+#                            |
+#                           [B]  <-- <unknown>
+#
+#
+#  Y_0 = W_0 * X_0 + B  => Linear equation
+
+#  Descr: 
+#
+#  [
+#    [CRIM_0, ZN_0, INDUS_0, CHAS_0, NOX_0, RM_0, ..., LSTAT_0],
+#    [CRIM_1, ZN_1, INDUS_1, CHAS_1, NOX_1, RM_1, ..., LSTAT_1],
+#    [CRIM_2, ZN_2, INDUS_2, CHAS_2, NOX_2, RM_2, ..., LSTAT_2],
+#    ...
+#    [CRIM_N-1, ZN_N-1, INDUS_N-1, CHAS_N-1, NOX_N-1, RM_N-1, ..., LSTAT_N-1]
+#  ]
+boston = load_boston()
+
+#  Input matrix: all avg. rooms per home
+#
+#  (5th index 5] of every row [: )
+#
+#  [RM_0, RM_1, RM_2, ..., RM_N-1]
+#
+#  equals
+#
+#  [X_0, X_1, X_2, ..., X_N-1]
+#
+#  NOTE: np.array() is just an array casting, as np is an improved library for matrix ops.
+X = np.array(boston.data[:, 5])
+
+#  Target matrix: avg. price per home
+Y = np.array(boston.target)
+
+plt.scatter(X, Y, alpha=0.5)
+
+#  Add a column full of 1s at the beginning
+#
+#  Which leaves us X:
+#
+#     [  1,    X_0]
+#     [  1,    X_1]     (X[0]: [  1,   1, ...,     1])
+#     [  1,    ...]     (X[1]: [X_0, X_1, ..., X_N-1])
+#     [  1,  X_N-1]
+X = np.array([np.ones(len(X)), X]).T
+
+#  Now we want to get the least mean squared error, which is: e = (y_o - y_expect)^2
+#
+#  (y_o - y_expect)^2 = y_o^2 - 2 * y_o * y_expect + y_expect^2
+# 
+#  Given: y_o = w * x + b => Y = WX (where W = [B, W] AND X = [1, X])
+# 
+#  e = (WX)^2 - 2WX + Y^2 -> e = 0 => W = (X^T * X)^-1 * X^T * Y
+B = np.linalg.inv(X.T @ X) @ X.T @ Y
+
+plt.plot([4, 9], [B[0] + B[1] * 4, B[0] + B[1] * 9], c='black')
+plt.show()
